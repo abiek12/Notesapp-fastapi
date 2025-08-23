50a7e1305d3b1a916aecee9b158ecf9c51c905fc
Abhishek <abhishekkanichery@gmail.com>
2025-08-23 22:31:09 +0530

    implemented serving static files and basic jinja templates

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
-from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-@app.get("/")
+@app.get("/health")
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+@app.get('/', response_class=HTMLResponse)
+def get_all_notes(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+    
