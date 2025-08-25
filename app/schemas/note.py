6bb0827da6874b39aa711deffe61123cea56c8cd
Abhishek <abhishekkanichery@gmail.com>
2025-08-26 00:02:00 +0530

    schema added

---
+++ app/schemas/note.py
@@ -0,0 +1,12 @@
+def noteEntity(item) -> dict:
+    return {
+        "id": str(item["_id"]),
+        "title": str(item["title"]),
+        "desc": str(item["desc"]),
+        "important": str(item["important"]),
+        "created_at": str(item["created_at"]),
+        "updated_at": str(item["updated_at"])
+    }
+    
+def notesEntity(itmes) -> list:
+    return [noteEntity(item) for item in itmes]
