d6547cc48d19a9fab490dd23b103c7c3a8b19e6a
Abhishek <abhishekkanichery@gmail.com>
2025-08-31 22:52:18 +0530

    update urls model

--- app/models/urls.py
+++ app/models/urls.py
@@ -2,9 +2,10 @@ from pydantic import BaseModel
 from datetime import datetime
 from app.db.connection import db
 
-class Note(BaseModel):
-    title: str
-    desc: str
-    important: bool = False
+class Urls(BaseModel):
+    id: str
+    shortend_url: str
+    original_url: str
+    userId: str
     created_at: str = str(datetime.timestamp(datetime.now()))
     updated_at: str = str(datetime.timestamp(datetime.now()))
